166425d23c7e09840629f5d17a2fcc6889ffd622
jiao <dxlcq@outlook.com>
2023-12-12 16:20:56 +0800

    规范格式

--- deque/deque.h
+++ deque/deque.h
@@ -6,25 +6,28 @@ namespace jiao{
 
 template <typename T>
 class deque : private list<T>{
-
+// 私有继承 不会继承基类的成员函数
 public:
     deque():list<T>(){};    // 直接调用父类的构造函数
     ~deque(){};             // 直接调用父类的析构函数
 
     void push_front(const T& val){
-        list<T>::insert(list<T>::begin(), val);
+        list<T>::push_front(val);
     };
     void push_back(const T& val){
-        list<T>::insert(list<T>::end()->next, val);
+        list<T>::push_back(val);
     };
     void pop_front(){
-        list<T>::erase(list<T>::begin());
+        list<T>::pop_front();
     };
     void pop_back(){
-        list<T>::erase(list<T>::end());
+        list<T>::pop_back();
     };
 
-    int size(){
+    void clear(){
+        list<T>::clear();
+    };
+    int size() const{
         return list<T>::size();
     };
 
--- list/list.h
+++ list/list.h
@@ -7,44 +7,47 @@ class list{
 
 public:
     struct Node{
-        T data;     // 数据域
-        Node *prev; // 前驱指针域
-        Node *next; // 后继指针域
+        T     data;         // 数据域
+        Node* prev;         // 前驱指针域
+        Node* next;         // 后继指针域
     };
 
-    list();     // 初始化链表
-    ~list();    // 销毁链表
+    list();                 // 初始化链表
+    ~list();                // 销毁链表
 
     void insert(Node* pos, const T& val);   // 在 pos 处插入元素
     void erase(Node* pos);                  // 删除 pos 处的元素
+    void push_front(const T& val);          // 在链表头部插入元素
+    void push_back(const T& val);           // 在链表尾部插入元素
+    void pop_front();                       // 删除链表头部元素
+    void pop_back();                        // 删除链表尾部元素
 
-    void clear();
-    int size();
+    void clear();           // 清空链表
+    int  size() const;      // 返回链表长度
 
-    Node* begin() const;            // 返回链表第一个元素指针
-    Node* end() const;              // 返回链表最后一个元素指针
-
+    Node* begin() const;    // 第一个元素指针
+    Node* end() const;      // 最后一个元素指针
 private:
-    Node* _head;        // 头指针（带头结点）
-    Node* _tail;        // 尾指针（最后一个结点的下一个）
-    int _size;          // 链表长度
+    Node* _head;            // 头指针（带头结点）
+    Node* _tail;            // 尾指针（最后一个元素的下一个）
+    int   _size;            // 链表长度
 };
 
 template <typename T>
 list<T>::list(){
-    _head = new Node;
-    _tail = new Node;
-    _head->prev = nullptr;
-    _head->next = _tail;
-    _tail->prev = _head;
-    _tail->next = nullptr;
-    _size = 0;
+    _head = new Node;       // 创建头结点
+    _tail = new Node;       // 创建尾结点
+    _head->prev = nullptr;  // 头结点无前驱
+    _head->next = _tail;    // 头结点后驱为尾结点
+    _tail->prev = _head;    // 尾结点前驱为头结点
+    _tail->next = nullptr;  // 尾结点无后继
+    _size = 0;              // 链表长度为 0
 }
 
 template <typename T>
 list<T>::~list(){
-    clear();                    // 清空链表
-    delete _head;               // 删除头指针
+    clear();                // 清空链表
+    delete _head;           // 删除头指针
 }
 
 template <typename T>
@@ -63,8 +66,11 @@ void list<T>::insert(Node* pos, const T& val){
 
 template <typename T>
 void list<T>::erase(Node* pos){
-    if(pos == nullptr) return ;     // pos 为空指针，直接返回
-    if(_size == 0) return ;         // 链表为空，直接返回
+    if(pos == nullptr)  return ;    // pos 为空指针，直接返回
+    if(pos == _head)    return ;    // pos 为头结点，直接返回
+    if(pos == _tail)    return ;    // pos 为尾结点，直接返回
+    if(_size == 0)      return ;    // 链表为空，直接返回
+
     pos->prev->next = pos->next;    // pos 的前驱结点的后继指针域指向 pos 的后继结点
     pos->next->prev = pos->prev;    // pos 的后继结点的前驱指针域指向 pos 的前驱结点
 
@@ -74,12 +80,33 @@ void list<T>::erase(Node* pos){
 
 
 template <typename T>
-void list<T>::clear(){
-
+void list<T>::push_front(const T& val){
+    insert(_head->next, val);       // 在头结点的后面插入元素
 }
 
 template <typename T>
-int list<T>::size(){
+void list<T>::push_back(const T& val){
+    insert(_tail->prev, val);       // 在尾结点的前面插入元素
+}
+
+template <typename T>
+void list<T>::pop_front(){
+    erase(_head->next);             // 删除头结点的后继结点
+}
+
+template <typename T>
+void list<T>::pop_back(){
+    erase(_tail->prev);             // 删除尾结点的前驱结点
+}
+
+template <typename T>
+void list<T>::clear(){
+    while(_size > 0)                // 链表长度大于 0
+        erase(_head->next);         // 删除头结点的后继结点
+}
+
+template <typename T>
+int list<T>::size() const{
     return _size;
 }
 
@@ -92,7 +119,5 @@ template <typename T>
 typename list<T>::Node* list<T>::end() const{
     return _tail->prev;
 }
-
-
 
 }   // namespace jiao
